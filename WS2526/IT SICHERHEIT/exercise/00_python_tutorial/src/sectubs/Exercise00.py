68d234404dea1e62ec4d3716d4a012ea5be87344
Khaled Houda <khaledohouda@gmail.com>
2026-02-13 15:55:16 +0100

    add readme

---
+++ WS2526/IT SICHERHEIT/exercise/00_python_tutorial/src/sectubs/Exercise00.py
@@ -0,0 +1,12 @@
+import datetime
+
+class Exercise00
+    STUDENT_NAME = "Khaled Houda"
+
+    deadline()
+        wantedDeadline = datetime.datetime(2023, 11, 15, 9, 0)
+        return x.strftime("%B %d, %H:%M, %Y")
+    deadline(string format)
+        wantedDeadline = datetime.datetime(2023, 11, 15, 9, 0)
+        return wantedDeadline.strftime(format)
+
---
+++ WS2526/README.md
@@ -0,0 +1,1 @@
+This repository serves as a study document and storage enviroment for my University material.
