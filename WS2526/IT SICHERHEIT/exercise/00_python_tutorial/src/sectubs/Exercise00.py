a0a1b0448912db0de590d207c965a4339f998beb
Khaled Houda <khaledohouda@gmail.com>
2026-02-16 16:11:15 +0100

    Exercise00

--- WS2526/IT SICHERHEIT/exercise/00_python_tutorial/src/sectubs/Exercise00.py
+++ WS2526/IT SICHERHEIT/exercise/00_python_tutorial/src/sectubs/Exercise00.py
@@ -1,12 +1,133 @@
 import datetime
+import string
+import os
+import glob
 
-class Exercise00
+
+class Exercise00:
     STUDENT_NAME = "Khaled Houda"
 
-    deadline()
-        wantedDeadline = datetime.datetime(2023, 11, 15, 9, 0)
-        return x.strftime("%B %d, %H:%M, %Y")
-    deadline(string format)
+    @staticmethod
+    def deadline(format):
         wantedDeadline = datetime.datetime(2023, 11, 15, 9, 0)
         return wantedDeadline.strftime(format)
+    
+    def __init__(self, bamboozle:str, *args):
+        self.first17Letters = ""
+        iterator = 17
+        
+        if len(bamboozle) < 17:
+            iterator = len(bamboozle)
+           
+        for i in range(iterator):
+            self.first17Letters += bamboozle[i]
+        self.first17Letters += "..."     
+    @property
+    def txt(self):
+        txt = ""
+        txt += self.first17Letters
+        return txt
 
+    def format(self, mode:str):
+        if mode == "order":
+            return OrderNiggaFuckNiggas()
+        if mode == 'dict':
+            return DictFuckEpsteinNiggas()
+        
+    def listfiles(self, type = ""):
+        if type != "":
+            typeSpecifier = "**/*" + '.'+ type
+        else:
+            typeSpecifier = "**/*"
+
+        yield glob.glob(typeSpecifier)
+
+
+
+
+
+
+            
+        
+        
+class OrderNiggaFuckNiggas:
+    def format(self,  par1:str , par2:str, par3:str):
+        orderedOutput = par3 + " - " + par2 + " - " + par1 + " epstein fuck niggas"
+        return orderedOutput
+        
+class DictFuckEpsteinNiggas:
+    def format(self, x:float , y:float):
+        x= str(x)
+        y= str(y)
+
+        xTemp = ""
+        yTemp = ""
+        foundPointX = False
+        foundPointY = False
+        for i in range(len(x)):
+            if x[i] == '.':
+                foundPointX = True
+            if i+1 == len(x):
+                xTemp += "."
+                xTemp += "0"
+                break
+
+
+            if foundPointX:
+                xTemp += x[i]
+                xTemp += x[i+1]
+                break
+
+            xTemp +=x[i]
+            
+        for i in range(len(y)):
+            if y[i] == '.':
+                foundPointY = True
+
+            if foundPointY:
+                yTemp += y[i]
+                if (len(y)-1) -i >= 4:
+                    yTemp += y[i+1]
+                    yTemp += y[i+2]
+                    yTemp += y[i+3]
+                    yTemp += y[i+4]
+                
+                if (len(y)-1) -i == 3:
+                    yTemp += y[i+1]
+                    yTemp += y[i+2]
+                    yTemp += y[i+3]
+                    yTemp += "0"
+
+                if (len(y)-1) -i == 2:
+                    yTemp += y[i+1]
+                    yTemp += y[i+2]                                        
+                    yTemp += "00"
+                
+                if (len(y)-1) -i == 1:
+                    yTemp += y[i+1]
+                    yTemp += y[i+2]                                        
+                    yTemp += "000"                
+                
+                break
+
+            yTemp +=y[i]
+
+    
+        x = xTemp
+        y = yTemp
+        return "x ,y = (" + x + " , " + y + ")"             
+
+    
+
+
+    
+
+
+
+
+ex = Exercise00("abcdefghijklmnopqrstuvwxyz")
+print(ex.txt)
+print( ex.format('order').format('baba', 'mama' , 'jido') )
+print(ex.format('dict').format( 1234, 6877.8245454533 ))
+for i in ex.listfiles():
+    print(i)
