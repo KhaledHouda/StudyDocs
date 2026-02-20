02e21ac7ed31cec0abee6c7cd1fd55d4a08ff104
Khaled Houda <khaledohouda@gmail.com>
2026-02-20 17:19:00 +0100

    work on ex01 part 4

--- WS2526/IT SICHERHEIT/exercise/01_crypto/src/mono/mono.py
+++ WS2526/IT SICHERHEIT/exercise/01_crypto/src/mono/mono.py
@@ -1,15 +1,27 @@
 import argparse
-parser = argparse.ArgumentParser()
-
-#mutually exclusive group for encrypt XOR decrypt within parser
-encr_decr = parser.add_mutually_exclusive_group()
-encr_decr.add_argument("--encrypt", help= "give key nigga")
-encr_decr.add_argument("--decrypt", help= "give key nigga")
+from pathlib import Path
 
 
-parser.add_argument("--out" , help = 'Outfile where')
-parser.add_argument("FILE")
-args = parser.parse_args()
+def encryptFile(FILE, key):
+        print()
+
+def decryptFile(FILE, key):
+        print()
+
+
+def main():   
+
+    parser = argparse.ArgumentParser()
+
+    #mutually exclusive group for encrypt XOR decrypt within parser
+    encr_decr = parser.add_mutually_exclusive_group()
+    encr_decr.add_argument("--encrypt", help= "give key nigga")
+    encr_decr.add_argument("--decrypt", help= "give key nigga")
+
+
+    parser.add_argument("--out" , help = 'Outfile where')
+    parser.add_argument("FILE")
+    args = parser.parse_args()
 
 
 
@@ -17,31 +29,29 @@ args = parser.parse_args()
 
 
 
-FILE = args.FILE
+    FILE = Path(args.FILE)
+    FILE_CONTENTS = Path.read_text(FILE)
+
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    mapper
 
 
-if args.encrypt:
+
+
+    if args.encrypt:
     #encryption process
-    encrypt()
+        encryptFile(1,1)
 
 
-if args.decrypt:
-    #decryption process
-    decrypt()
-
-
-
-
-def encrypt(FILE, key):
-    
-
-def decrypt(FILE, key):
-
+    if args.decrypt:
+        #decryption process
+        decryptFile(1,1)
 
 
     
 
-
+if __name__  == "__main__":
+    main()
 
 
 
